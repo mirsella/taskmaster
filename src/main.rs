7b458432d42c068c64d656069754374269601ee4
mirsella <mirsella@protonmail.com>
2024-02-21 16:04:12 +0100

    remove unneeded code

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-mod parsing_conf;
+mod config;
 
-use log::warn;
-use parsing_conf::{get_config, Config};
+use log::{debug, error, warn};
 use std::{env::args, error::Error, os::unix::process::CommandExt, process::Command};
 use users::get_current_uid;
 
+use crate::config::get_config;
+
 fn privilege_descalation(user: &str) -> Result<(), String> {
     if get_current_uid() != 0 {
+        debug!(
+            "running as non-privilaged user {}",
+            users::get_current_username()
+                .unwrap_or("deleted".into())
+                .to_string_lossy()
+        );
         return Ok(());
     }
     warn!("This program should not be run as root. relaunching as {user}");
@@ -31,16 +38,17 @@ fn privilege_descalation(user: &str) -> Result<(), String> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let conf: Config = match get_config() {
-		Ok(v) => v,
-		Err(e) => {
-			eprintln!("Error while parsing the configuration file: {:#?}", e);
-			return e;
-		}
-	};
-	dbg!(conf);
-
-    privilege_descalation("mirsella")?;
-    // privilege_descalation(conf.user);
+    // TODO: start syslog and simple_logger
+    let conf = match get_config() {
+        Ok(v) => v,
+        Err(e) => {
+            error!("parsing the configuration file: {:#?}", e);
+            return Err(e.into());
+        }
+    };
+    if let Err(e) = privilege_descalation(&conf.user) {
+        error!("descalating privileges: {:#?}", e);
+        return Err(e.into());
+    }
     Ok(())
 }
