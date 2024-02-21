c2277cf3b64d4e1913e87c666d970212b4199cee
Nathan Guiard <nguiard@student.42.fr>
2024-02-21 16:04:42 +0100

    Path to config file givable by command line argument

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -6,14 +6,14 @@
 /*   By: nguiard <nguiard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:26:32 by nguiard           #+#    #+#             */
-/*   Updated: 2024/02/21 14:55:39 by nguiard          ###   ########.fr       */
+/*   Updated: 2024/02/21 16:03:34 by nguiard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 pub mod data_type;
 pub mod signal;
 
-use std::fs;
+use std::{fs, path::Path};
 use data_type::Config;
 
 /// Returns the configuration found in the TOML configuration file
@@ -21,8 +21,8 @@ use data_type::Config;
 /// `Ok()` -> `parsing_conf::Config` with the configuration parsed
 ///
 /// `Err()` -> `String` that describes the problem
-pub fn get_config() -> Result<Config, String> {	
-	let raw_file = match fs::read_to_string("config/default.toml") {
+pub fn get_config(file_path: &Path) -> Result<Config, String> {	
+	let raw_file = match fs::read_to_string(file_path) {
 		Ok(v) => v,
 		Err(e) => return Err(e.to_string()),
 	};
@@ -37,11 +37,13 @@ pub fn get_config() -> Result<Config, String> {
 
 #[cfg(test)]
 mod parsing_tests {
+    use std::path::Path;
+
     use super::get_config;
 
 	#[test]
 	fn basic_config() {
-		match get_config() {
+		match get_config(Path::new("taskmaster.toml")) {
 			Ok(var) => println!("No problem"),
 			Err(e) => panic!()
 		}
--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,30 @@
 /*   By: nguiard <nguiard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:09:10 by nguiard           #+#    #+#             */
-/*   Updated: 2024/02/21 14:57:25 by nguiard          ###   ########.fr       */
+/*   Updated: 2024/02/21 15:59:57 by nguiard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 mod config;
+use std::env;
+use std::path::Path;
+
 use config::get_config;
 use config::data_type::Config;
 
 fn main() {
-	let conf: Config = match get_config() {
+	let location: Vec<String> = env::args().collect();
+
+	let conf_file: &Path = match location.len() {
+		1 => Path::new("taskmaster.toml"),
+		_ => Path::new(&location[1])
+	};
+
+	let conf: Config = match get_config(conf_file) {
 		Ok(v) => v,
 		Err(e) => {
-			eprintln!("Error while parsing the configuration file: {:#?}", e);
+			eprintln!("Error while parsing the configuration file {:?}: {:#?}",
+						conf_file, e);
 			return ;
 		}
 	};
