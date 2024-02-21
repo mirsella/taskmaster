14f3353ef20cc843a2a3df8c0493f9bd124707c2
mirsella <mirsella@protonmail.com>
2024-02-21 15:14:01 +0100

    add privilege descalation

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-mod config;
-use config::get_config;
-use config::data_type::Config;
+mod parsing_conf;
 
-fn main() {
+use log::warn;
+use parsing_conf::{get_config, Config};
+use std::{env::args, error::Error, os::unix::process::CommandExt, process::Command};
+use users::get_current_uid;
+
+fn privilege_descalation(user: &str) -> Result<(), String> {
+    if get_current_uid() != 0 {
+        return Ok(());
+    }
+    warn!("This program should not be run as root. relaunching as {user}");
+    let user = users::get_user_by_name(user).ok_or(format!("User {} not found", user))?;
+    Err(Command::new(args().next().unwrap())
+        .uid(user.uid())
+        .gid(user.primary_group_id())
+        .exec()
+        .to_string())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
 	let conf: Config = match get_config() {
 		Ok(v) => v,
 		Err(e) => {
 			eprintln!("Error while parsing the configuration file: {:#?}", e);
-			return ;
+			return e;
 		}
 	};
-
 	dbg!(conf);
+
+    privilege_descalation("mirsella")?;
+    // privilege_descalation(conf.user);
+    Ok(())
 }
