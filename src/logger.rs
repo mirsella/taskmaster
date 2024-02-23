78136260d52066216bd76e64e8d955b15155fcbc
mirsella <mirsella@protonmail.com>
2024-02-23 09:50:49 +0100

    improve logger, use RUST_LOG to overwrite config log level

--- src/logger.rs
+++ src/logger.rs
@@ -10,27 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-use std::{env, fs::File, io::Write, path::Path};
+use std::{fs::File, io::Write, path::Path, time::Duration};
 use tracing::Level;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::{
     fmt::{self, Subscriber},
     layer::SubscriberExt,
+    EnvFilter,
 };
 
 pub fn init_logger(
     log_file: &Path,
     log_level: &Level,
 ) -> Result<WorkerGuard, Box<dyn std::error::Error>> {
-    let log_level = env::var("LOG_LEVEL")
-        .map(|str| str.parse::<Level>().unwrap_or(*log_level))
-        .unwrap_or(*log_level);
     let mut file = File::options().append(true).create(true).open(log_file)?;
-    file.write_all(b"\n")?;
+    if file.metadata()?.created()?.elapsed()? > Duration::from_secs(1) {
+        file.write_all(b"\n")?;
+    }
     let (file_writer, file_guard) = tracing_appender::non_blocking(file);
-
     let subscriber = Subscriber::builder()
-        .with_max_level(log_level)
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new(log_level.as_str())),
+        )
         .finish()
         .with(tracing_journald::layer()?)
         .with(fmt::layer().with_writer(file_writer));
