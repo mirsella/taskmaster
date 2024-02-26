59d6c8fa23f9e5365f8fddad35db7623f8de477d
Nathan Guiard <nguiard@student.42.fr>
2024-02-26 16:02:44 +0100

    Little changes

--- src/tui.rs
+++ src/tui.rs
@@ -15,7 +15,7 @@ use ratatui::{
     widgets::{Block, Borders, Paragraph, Row, Table},
     Terminal,
 };
-use std::{fmt::Write, io, panic, str::FromStr};
+use std::{io, panic, str::FromStr};
 use tracing::{trace, Level};
 use tui_input::{backend::crossterm::EventHandler, Input};
 use tui_logger::TuiLoggerWidget;
@@ -220,7 +220,9 @@ fn status(programs: &[Program]) -> Table {
     // TODO: add a row for each program
     // https://docs.rs/ratatui/latest/ratatui/widgets/struct.Table.html
 	for i in 0..programs.len() {
-		rows.push(programs[i].status());
+		for row in programs[i].status() {
+			rows.push(row);
+		}
 	}
 	Table::new(rows, &[])
 }
